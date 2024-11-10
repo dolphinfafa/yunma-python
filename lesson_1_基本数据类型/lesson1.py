7bf0bd5097130bee91b973e7576f1d623bd48b24
dolphinfafa <leafleaving@gmail.com>
2024-11-10 10:58:34 +0800

    modify print method with %

--- lesson_1_基本数据类型/lesson1.py
+++ lesson_1_基本数据类型/lesson1.py
@@ -4,5 +4,8 @@ month = 11
 day = 9
 name = '小明'
 
-#打印
+#打印%字符串
+print('我在%d年%d月%02d日开始python的学习，我-%s，一定能学好'%(year,month,day,name))
+
+#打印format字符串
 print('我在{:d}年{:d}月{:02d}日开始python的学习，我-{:s}，一定能学好'.format(year,month,day,name))
